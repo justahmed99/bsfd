3a63aeed2a0175fec00156675da6224e133e72ba
Ahmad M <ahmad.kirghizia@gmail.com>
2018-06-03 00:12:15 +0700

    Rombak

--- myFunction.py
+++ myFunction.py
@@ -2,8 +2,12 @@ import cv2 as cv
 
 # fungsi yang digunakan
 def imgDiff(f0, f1, f2) :
-    diff1 = cv.absdiff(f2, f1)
-    diff2 = cv.absdiff(f1, f2)
+    diff1 = cv.absdiff(f1, f0)
+    diff2 = cv.absdiff(f2, f1)
+    gs1 = imgGrayscaling(diff1)
+    gs2 = imgGrayscaling(diff2)
+    _, otsu1 = cv.threshold(gs1, 50, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+    __, otsu2 = cv.threshold(gs2, 50, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
     return cv.bitwise_and(diff1, diff2)
 
 def imgSub(f1, f0) :
@@ -16,7 +20,7 @@ def adaptiveThreshold(f) :
     return cv.adaptiveThreshold(f, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 115, 1)
 
 def otsuThresholding(f):
-    return cv.threshold(f, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+    return cv.threshold(f, 50, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
 
 def showVideo(name, source) :
     cv.namedWindow(name, cv.WINDOW_NORMAL)
--- program.py
+++ program.py
@@ -2,54 +2,61 @@ import cv2 as cv
 import myFunction as mF
 
 #inisiasi kamera
-cam = cv.VideoCapture(0)
+cam = cv.VideoCapture(1)
 
 # Deklarasi pengambilan pertama frame f(i-5), f(i), dan f(i+5)
-fMin5Input = mF.imgGrayscaling(cam.read()[1])
-fInput = mF.imgGrayscaling(cam.read()[1])
-fPlus5Input = mF.imgGrayscaling(cam.read()[1])
+fMin5Input = cv.blur(cam.read()[1], (5,5))
+fInput = cv.blur(cam.read()[1], (5,5))
+fPlus5Input = cv.blur(cam.read()[1], (5,5))
 
 # Penerapan adaptive threshold mean c
-fMin5 = mF.adaptiveThreshold(fMin5Input)
-f = mF.adaptiveThreshold(fInput)
-fPlus5 = mF.adaptiveThreshold(fPlus5Input)
+#ret1, fMin5 = mF.otsuThresholding(fMin5Input)
+#ret2, f = mF.otsuThresholding(fInput)
+#ret3, fPlus5 = mF.otsuThresholding(fPlus5Input)
 
 # Deklarasi awal gambar referensi
-imageRef = f    # jangan diubah
+ret, imageRef = mF.otsuThresholding(mF.imgGrayscaling(fInput))    # jangan diubah
 count = 0
 while True :
     #display gambar
-    mF.showVideo("Binary image", mF.imgDiff(fMin5, f, fPlus5))
+    #mF.showVideo("Binary image", mF.imgDiff(fMin5, f, fPlus5))
+    #mF.showVideo('camera', cam.read()[1])
+    if count == 100 :
+        refBlur = cv.blur(cam.read()[1], (5,5))
+        _, imageRef = mF.otsuThresholding(mF.imgGrayscaling(refBlur))
+        print('reference image is captured.')
+        count = 0
     mF.showVideo('referensi', imageRef)
 
     # pengambilan nilai matriks D(i-5) dan D(i+5) dengan fungsi imgDiff kemudian
     # penentuan objek bergerak
-    movObject = mF.imgDiff(fMin5, f, fPlus5)
-
+    movObject = mF.imgDiff(fMin5Input, fInput, fPlus5Input)
+    mF.showVideo('movObject', movObject)
     #penentuan background image
-    bGround = mF.imgSub(movObject, f)
+    #bGround = mF.imgSub(movObject, imageRef)
     #mF.showVideo('bGround', bGround)
 
     # generate Revised Background Image (RBI)
-    RBI = cv.bitwise_and(imageRef, bGround)
-    imageRef = RBI
-    mF.showVideo('rbi', RBI)
+    #RBI = cv.bitwise_and(imageRef, bGround)
+    #imageRef = RBI
+    #mF.showVideo('rbi', RBI)
 
     # pendeteksian objek bergerak
-    mov = cv.absdiff(RBI, f)
-    mF.showVideo('mov', mov)
-    mF.showVideo('f', f)
-    if mov.all() != 0 :
-        count += 1
-        print("Warning, ", count)
+    #mov = cv.absdiff(RBI, f)
+    #mF.showVideo('mov', mov)
+    #mF.showVideo('f', imageRef)
+    #if mov.all() != 0 :
+    #    count += 1
+    #    print("Warning, ", count)
     # swap frame
-    fMin5 = f
-    f = fPlus5
-
-    fPlus5 = mF.adaptiveThreshold(mF.imgGrayscaling(cam.read()[1]))
-
+    fMin5Input = fInput
+    fInput = fPlus5Input
+    fPlus5 = cv.blur(cam.read()[1], (5,5))
+    #ret4, fPlus5 = mF.otsuThresholding(mF.imgGrayscaling(fPlusBlur))
+    count = count + 1
+    #print(count)
     key = cv.waitKey(10)
-    if key == 27 :
+    if key == 27 or key == ord('q'):
         cv.destroyAllWindows()
         break
 
